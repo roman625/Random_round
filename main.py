367303156b6f232c16c327c877dffd6704b626db
roman625 <romashka010404@gmail.com>
2020-11-21 13:08:38 +0500

    initial commit3

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPainter
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
-from random import random
+from random import random, randrange
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
 
 
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(130, 260, 171, 23))
+        self.pushButton.setObjectName("pushButton")
 
-class Window(QWidget):
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Нажми"))
+
+class Window(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('C:/Users/1/Desktop/PyCharm/UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
         self.fl = False
 
@@ -23,13 +39,14 @@ class Window(QWidget):
             d, x, y = round(random() * self.width()), round(random() * self.width() / 2), round(
                 random() * self.width() / 2)
             l = [x, y, d, d]
+            color = QColor(randrange(256), randrange(256), randrange(256))
             qp = QPainter()
             qp.begin(self)
 
-            qp.setBrush(Qt.yellow)
+            qp.setBrush(color)
             qp.drawEllipse(*l)
-            qp.end()
 
+            qp.end()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
